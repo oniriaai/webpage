08e0a201a1d995e5b8e6a372baed78eec405a191
Pietro <pmunoz3505@gmail.com>
2026-03-11 14:37:25 +0100

    updated navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,12 +2,16 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between p-6">
-      <div className="font-bold">ONIRIASOLUTIONS</div>
+    <nav className="flex justify-between p-6 overflow-auto">
+        <Link className="font-bold flex items-center text-lg" href="/">
+        <img src="/icon.svg" className="h-9 w-auto mr-3"/> 
+        ONIRIASOLUTIONS
+        </Link>
+      
+      
 
       <div className="flex gap-6">
-        <Link href="/">Home</Link>
-        <Link href="/solutions">Solutions</Link>
+        <Link href="/solutions" className="align-middle">Solutions</Link>
         <Link href="/contact">Contact</Link>
       </div>
     </nav>
